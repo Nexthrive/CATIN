d9f87c5ade5e17d902bf0c7ec206a3c974595353
Edgar <edgarmasstudio@gmail.com>
2025-05-12 14:14:16 +0700

    ADMIN VIEW | Desktop Fisnished

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -25,128 +25,225 @@ export default function AdminPage({ children }: { children: React.ReactNode }) {
   return (
 <div className="flex bg-white">
 
-    <SidebarProvider className="w-fit bg-white">
+    <SidebarProvider className="w-[280px] bg-white">
       <AppSidebar />
-      <main className="">
-        
+      <main className="w-[280px]">
         {children}
       </main>
     </SidebarProvider>
   
 
 
-    <div className="container bg-whit px-[72px]  font-[family-name:var(--font-poppins)] py-10">
+    <div className="container bg-[#F8F8F8] px-8 py-[54px] gap-8 flex flex-col font-[family-name:var(--font-poppins)] ">
         <div className="flex justify-between items-center">
-            <div className="">
-                <h1>Hello Admin!</h1>
-                <p>Track the performance of Catin’s template at a glance!</p>
+            <div className="flex flex-col gap-2">
+                <h1 className="leading-10 tracking-tighter font-bold text-[40px]">Hello Admin!</h1>
+                <p className="leading-8 text-base font-normal text-[#848484]">Track the performance of Catin’s template at a glance!</p>
             </div>
-            <div className="flex">
-                <p>{new Date().toLocaleDateString('en-UK', { 
+            <div className="flex items-center gap-2">
+                <p className="leading-8 font-medium text-base">{new Date().toLocaleDateString('en-UK', { 
                     day: 'numeric', 
                     month: 'long', 
                     year: 'numeric' 
                 }).replace(' ', ', ')}</p>
-                <Image src='/Calender.svg' width={20} height={20} alt="calender"/>
+                <div  className="bg-[#E8E8E8] p-3 rounded-full">
+
+                <Image  src='/Calender.svg' width={24} height={24} className="h-6" alt="calender"/>
+                </div>
             </div>
         </div>
-      <Card className="border-0 bg-white shadow-none">
-        <CardHeader className=" px-10 gap-0">
-            <div className="flex items-center justify-between  border-[#E5E5EF] border-b-2 pb-4">
-
-        <div className="flex  flex-col">
-
-        <CardTitle className="text-[18px] text-[#9291A5] leading-5 font-normal tracking-normal">Statistics</CardTitle>
-        <CardDescription className="text-[22px] text-[#1E1B39] font-semibold leading-7">Sales report</CardDescription>
-        </div>
-          <div className="flex gap-4">
-            <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
-              <span className="w-2 h-2 rounded-full bg-[#962DFF]" />
-              <span className="text-xs text-[#1E1B39] leading-3.5">Product sales</span>
-            </div>
-            <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
-              <span className="w-2 h-2 rounded-full bg-[#FF718B]" />
-              <span className="text-xs text-[#1E1B39] leading-3.5">Subscription sales</span>
-            </div>
-            <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
-              <span className="w-2 h-2 rounded-full bg-[#93AAFD]" />
-              <span className="text-xs text-[#1E1B39] leading-3.5">Other sales</span>
+        <div className="flex flex-col gap-5">
+          <div className="text-card-foreground flex flex-col gap-6 p-8 rounded-xl  shadow-sm border-0 bg-white">
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-6 justify-around">
+                <div className="border p-[25px] flex flex-col gap-2 w-[80%] border-[#E5E5EF] rounded-md">
+                  <p className="text-[#9291A5] leading-[21px] text-sm  font-normal">Total Revenue</p>
+                  <h1 className="leading-12 font-semibold text-[32px] text-[#1E1B39]">$2.5M</h1>
+                  <div className="flex items-start gap-3">
+                  <Image src="/UpGreen.svg" className="pt-1.5" width={8} height={4} alt="UpGreen"/>
+                  <p className="text-[#22C55E] leading-[21px] font-normal text-sm">12.5%</p>
+                  </div>
+                </div>
+                <div className="border p-[25px] flex flex-col gap-2 w-[80%] border-[#E5E5EF] rounded-md">
+                  <p className="text-[#9291A5] leading-[21px] text-sm  font-normal">Total Revenue</p>
+                  <h1 className="leading-12 font-semibold text-[32px] text-[#1E1B39]">$2.5M</h1>
+                  <div className="flex items-start gap-3">
+                  <Image src="/UpGreen.svg" className="pt-1.5" width={8} height={4} alt="UpGreen"/>
+                  <p className="text-[#22C55E] leading-[21px] font-normal text-sm">12.5%</p>
+                  
+                  </div>
+                </div>
+                <div className="border p-[25px] flex flex-col gap-2 w-[80%] border-[#E5E5EF] rounded-md">
+                  <p className="text-[#9291A5] leading-[21px] text-sm  font-normal">Total Revenue</p>
+                  <h1 className="leading-12 font-semibold text-[32px] text-[#1E1B39]">$2.5M</h1>
+                  <div className="flex items-end gap-3">
+                  <Image src="/DownRed.svg" className="pb-2" width={8} height={4} alt="DownRed"/>
+                  <p className="text-[#EF4444] leading-[21px] font-normal text-sm">3.1%</p>
+                  </div>
+                
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <div className="border p-[24px] flex flex-col gap-6 w-[80%] border-[#E5E5EF] rounded-md">
+                  <div className="flex items-center justify-between">
+                    <h1 className="leading-7 font-semibold text-[18px] text-[#1E1B39]">Recent Orders</h1>
+                    <p className="text-[#4318D1] font-normal text-sm leading-[21px]">View All</p>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                  <div className="flex items-center justify-between">
+                    <div className="flex gap-3 items-center">
+                      <Image src="/40PXTemplate.svg" className="" width={40} height={40} alt="Template"/>
+                      <div className="">
+                        <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">Premium Template</p>
+                        <p className="text-[#9291A5] leading-[18px] font-normal text-xs">2 minutes ago</p>
+                      </div>
+                    </div>
+                    <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">$59.00</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex gap-3 items-center">
+                      <Image src="/40PXTemplate.svg" className="" width={40} height={40} alt="Template"/>
+                      <div className="">
+                        <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">Basic Template</p>
+                        <p className="text-[#9291A5] leading-[18px] font-normal text-xs">5 minutes ago</p>
+                      </div>
+                    </div>
+                    <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">$29.00</p>
+                  </div>
+                  </div>
+                  
+                </div>
+      
+                <div className="border p-[24px] flex flex-col gap-6 w-[80%] border-[#E5E5EF] rounded-md">
+                <div className="flex items-center justify-between">
+                  <h1 className="leading-7 font-semibold text-[18px] text-[#1E1B39]">Recent Users</h1>
+                  <p className="text-[#4318D1] font-normal text-sm leading-[21px]">View All</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-3 items-center">
+                    <Image src="/40PXTemplate.svg" className="rounded-full" width={40} height={40} alt="Template"/>
+                    <div className="">
+                      <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">John Smith</p>
+                      <p className="text-[#9291A5] leading-[18px] font-normal text-xs">Premium User</p>
+                    </div>
+                  </div>
+                  <p className="text-[#22C55E] leading-[21px] font-medium text-sm">Active</p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-3 items-center">
+                    <Image src="/40PXTemplate.svg" className="rounded-full" width={40} height={40} alt="Template"/>
+                    <div className="">
+                      <p className="text-[#1E1B39] leading-[21px] font-medium text-sm">Sarah Johnson</p>
+                      <p className="text-[#9291A5] leading-[18px] font-normal text-xs">Basic User</p>
+                    </div>
+                  </div>
+                  <p className="text-[#22C55E] leading-[21px] font-medium text-sm">Active</p>
+                </div>
+                </div>
+                
+              </div>
+              </div>
             </div>
           </div>
+          <Card className="border-0 bg-white">
+            <CardHeader className=" px-10 gap-0">
+                <div className="flex items-center justify-between  border-[#E5E5EF] border-b-2 pb-3">
+
+            <div className="flex  flex-col">
+
+            <CardTitle className="text-[18px] text-[#9291A5] leading-5 font-normal tracking-normal">Statistics</CardTitle>
+            <CardDescription className="text-[22px] text-[#1E1B39] font-semibold leading-7">Sales report</CardDescription>
             </div>
-        </CardHeader>
+              <div className="flex gap-4">
+                <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
+                  <span className="w-2 h-2 rounded-full bg-[#962DFF]" />
+                  <span className="text-xs text-[#1E1B39] leading-3.5">Product sales</span>
+                </div>
+                <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
+                  <span className="w-2 h-2 rounded-full bg-[#FF718B]" />
+                  <span className="text-xs text-[#1E1B39] leading-3.5">Subscription sales</span>
+                </div>
+                <div className="flex items-center gap-2 pl-4 pr-3 py-2 rounded-md border border-[#E5E5EF] bg-white">
+                  <span className="w-2 h-2 rounded-full bg-[#93AAFD]" />
+                  <span className="text-xs text-[#1E1B39] leading-3.5">Other sales</span>
+                </div>
+              </div>
+                </div>
+            </CardHeader>
+            
+            <CardContent className="p-0">
         
-        <CardContent className="p-0">
-     
-          <div className="h-[400px]">
-            <ResponsiveContainer width="100%" height="100%">
-              <LineChart
-                data={data}
-                margin={{
-                  top: 20,
-                  right: 30,
-                  left: 20,
-                  bottom: 10,
-                }}
-              >
-                <CartesianGrid   horizontal={true} 
-                vertical={false}  strokeDasharray="3 3" // This creates the dotted effect (3px dash, 3px gap)
-                />
-                                <XAxis 
-                                dataKey="month" 
-                                axisLine={false}
-                                tickLine={false}
-                                />
-                <YAxis
-                tickLine={false}
-                axisLine={false}
-                domain={[0, 'dataMax']} // Slightly extend beyond max data
-                tickCount={10} // Request exactly 6 ticks
-                tickFormatter={(value) => {
-                    if (value >= 1000000) return `${value/1000000}m`;
-                    if (value >= 1000) return `${value/1000}k`;
-                    return value.toString();
-                }}
-                />
+              <div className="h-[400px]">
+                <ResponsiveContainer width="100%" height="100%">
+                  <LineChart
+                    data={data}
+                    margin={{
+                      top: 20,
+                      right: 30,
+                      left: 20,
+                      bottom: 10,
+                    }}
+                  >
+                    <CartesianGrid   horizontal={true} 
+                    vertical={false}  strokeDasharray="3 3" // This creates the dotted effect (3px dash, 3px gap)
+                    />
+                                    <XAxis 
+                                    dataKey="month" 
+                                    axisLine={false}
+                                    tickLine={false}
+                                    />
+                    <YAxis
+                    tickLine={false}
+                    axisLine={false}
+                    domain={[0, 'dataMax']} // Slightly extend beyond max data
+                    tickCount={10} // Request exactly 6 ticks
+                    tickFormatter={(value) => {
+                        if (value >= 1000000) return `${value/1000000}m`;
+                        if (value >= 1000) return `${value/1000}k`;
+                        return value.toString();
+                    }}
+                    />
 
-                <Tooltip 
-                formatter={(value: number) => new Intl.NumberFormat('en').format(value)}
-                />
-                            
-                <Line
-                  type="monotone"
-                  dataKey="productSales"
-                  stroke="#962DFF"
-                  strokeWidth={3}
-                  dot={false}
-                  strokeDasharray="20 8"
-                  activeDot={{ r: 8 }}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="subscriptionSales"
-                  stroke="#FF718B"
-                  strokeWidth={3}
-                  dot={false}
-                  strokeDasharray="20 8"
-                  activeDot={{ r: 8 }}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="otherSales"
-                  stroke="#93AAFD"
-                  strokeWidth={3}
-                  dot={false}
-                  strokeDasharray="20 8"
-                  activeDot={{ r: 8 }}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-        </CardContent>
-      </Card>
+                    <Tooltip 
+                    formatter={(value: number) => new Intl.NumberFormat('en').format(value)}
+                    />
+                                
+                    <Line
+                      type="monotone"
+                      dataKey="productSales"
+                      stroke="#962DFF"
+                      strokeWidth={3}
+                      dot={false}
+                      strokeDasharray="20 8"
+                      activeDot={{ r: 8 }}
+                    />
+                    <Line
+                      type="monotone"
+                      dataKey="subscriptionSales"
+                      stroke="#FF718B"
+                      strokeWidth={3}
+                      dot={false}
+                      strokeDasharray="20 8"
+                      activeDot={{ r: 8 }}
+                    />
+                    <Line
+                      type="monotone"
+                      dataKey="otherSales"
+                      stroke="#93AAFD"
+                      strokeWidth={3}
+                      dot={false}
+                      strokeDasharray="20 8"
+                      activeDot={{ r: 8 }}
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
     </div>
-    <div className="min-h-svh bg-white min-w-[400px] border-l border-l-[#E5E5EF]">
+    <div className="min-h-svh bg-white min-w-[380px] border-l border-l-[#E5E5EF]">
 
     </div>
 </div>
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+import { Calendar, Home, ChartLine,Hexagon, ImageIcon, Layers } from "lucide-react"
+import Image from "next/image"
 import {
   Sidebar,
   SidebarContent,
@@ -14,46 +14,109 @@ import {
 // Menu items.
 const items = [
   {
-    title: "Home",
+    title: "Dashboard",
     url: "#",
-    icon: Home,
+    image: "/CatinAdminLogoOutline.svg",
+    active: true,
   },
   {
-    title: "Inbox",
+    title: "Analytics",
     url: "#",
-    icon: Inbox,
+    image: "/AnalyticsIcon.svg",
+    active: false,
+
+  },
+ 
+ 
+]
+const items2 = [
+  {
+    title: "Components",
+    url: "#",
+    image: "/ComponentIcon.svg",
+    active: false,
+
   },
   {
-    title: "Calendar",
+    title: "Styles",
     url: "#",
-    icon: Calendar,
+    image: Hexagon,
+    active: false,
+
+  },
+]
+
+const items3 = [
+  {
+    title: "Images",
+    url: "#",
+    image: ImageIcon,
+    active: false,
+
   },
   {
-    title: "Search",
+    title: "Icons",
     url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    image: Layers,
+    active: false,
+
   },
 ]
 
 export function AppSidebar() {
   return (
-    <Sidebar className="bg-white">
+    
+    <Sidebar side="left" className="fixed bg-white w-[280px] font-[family-name:var(--font-poppins)]">
       <SidebarContent className="bg-white">
-        <SidebarGroup className="bg-white">
-          <SidebarGroupLabel className="font-[family-name:var(--font-poppins)] text-black font-bold text-2xl">Catin AP</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
+        <SidebarGroup className="bg-white p-0">
+          <div className="flex py-8 pl-8 border-b">
+            <div className="bg-[#4318D1] p-2 flex h-fit w-fit rounded-md justify-center">
+              <Image src="/CatinAdminLogo.svg" className="" width={15} height={15} alt="CatinAdmin"/>
+            </div>
+            <SidebarGroupLabel className="font-[family-name:var(--font-poppins)] text-black leading-[30px] px-3 font-bold text-xl">
+           Catin Admin</SidebarGroupLabel>
+          </div>
+          <SidebarGroupContent className="p-4">
+            <SidebarMenu className="gap-1">
+              <p className="leading-[18px] ml-3 tracking-wider font-medium text-[#9291A5] text-xs mb-1">MAIN</p>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                <SidebarMenuItem className={item.active ? "bg-[#F5F1FF] rounded-sm pl-3 py-2 pr-2" : "pl-3 py-2 pr-2 rounded-sm hover:bg-zinc-400/40"} key={item.title}>
+                  <SidebarMenuButton className="p-0 hover:bg-transparent" asChild>
                     <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
+                    
+                        <Image src={item.image} width={20} height={20} alt={`${item.title} icon`} />
+                    
+                      <span className={item.active ? "font-medium text-sm leading-[21px] text-[#4318D1]" : "font-medium text-sm leading-[21px] text-[#615E83]"}>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+               <p className="leading-[18px] ml-3 tracking-wider font-medium text-[#9291A5] mt-8 text-xs mb-1">DESIGN</p>
+              {items2.map((item2) => (
+                 <SidebarMenuItem className={item2.active ? "bg-[#F5F1FF] rounded-sm pl-3 py-2 pr-2" : "pl-3 py-2 pr-2 rounded-sm hover:bg-zinc-400/40"} key={item2.title}>
+                  <SidebarMenuButton className="p-0 hover:bg-transparent" asChild>
+                    <a href={item2.url}>
+                      {typeof item2.image === 'string' ? (
+                        <Image src={item2.image} width={15} height={15} alt={`${item2.title} icon`} />
+                      ) : (
+                        <item2.image size={20} />
+                      )}
+                      <span>{item2.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+               <p className="leading-[18px] ml-3 tracking-wider font-medium text-[#9291A5] mt-8 text-xs mb-1">ASSETS</p>
+              {items3.map((item3) => (
+                  <SidebarMenuItem className={item3.active ? "bg-[#F5F1FF] rounded-sm pl-3 py-2 pr-2" : "pl-3 py-2 pr-2 rounded-sm hover:bg-zinc-400/40"} key={item3.title}>
+                  <SidebarMenuButton className="p-0 hover:bg-transparent" asChild>
+                    <a href={item3.url}>
+                      {typeof item3.image === 'string' ? (
+                        <Image src={item3.image} width={15} height={15} alt={`${item3.title} icon`} />
+                      ) : (
+                        <item3.image size={20} />
+                      )}
+                      <span>{item3.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
@@ -63,5 +126,6 @@ export function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
+ 
   )
 }
